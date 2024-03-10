742f0a8e5f1059621641d6de3fe62790a1c69b59
renhubgit <renik2001@gmail.com>
2024-03-10 15:52:56 +0100

    main page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,33 @@ export default function Home() {
   return (
     <main className="relative h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/25 to-black">
       <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
-      <div className="h-screen flex justify-center items-center gap-3">
-        {appRoutes.map((route) => (
-          <Link key={route} href={route}>
-            {route}
-          </Link>
-        ))}
+      <div className="flex flex-col justify-center items-center h-full gap-16">
+        {/* Navigation */}
+        <nav className="animate-fade-in flex gap-8">
+          {appRoutes.map((route) => (
+            <Link
+              key={route}
+              href={route}
+              className="capitalize text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+            >
+              {route}
+            </Link>
+          ))}
+        </nav>
+
+        {/* Title */}
+        <div className="text-center">
+          <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+          <h1 className="z-10 font-calsans text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+            renkzk
+          </h1>
+          <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        </div>
+
+        {/* Scroll down button */}
+        <button className="animate-fade-in text-sm duration-500 text-zinc-500 hover:text-zinc-300">
+          {/* Scroll down » */}
+        </button>
       </div>
     </main>
   )
