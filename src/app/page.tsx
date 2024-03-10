2dd80f478eceac4f24046a286ada7bd77616ab3c
renhubgit <renik2001@gmail.com>
2024-03-10 15:14:23 +0100

    added particles component background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Particles from "@/components/ui/particles"
 import { AppRoute } from "@/enums/app-routes.enum"
 import Link from "next/link"
 
@@ -6,12 +7,15 @@ export default function Home() {
   const appRoutes = Object.values(AppRoute).filter((route) => route !== AppRoute.Home)
 
   return (
-    <main className="h-screen flex justify-center items-center gap-3">
-      {appRoutes.map((route) => (
-        <Link key={route} href={route}>
-          {route}
-        </Link>
-      ))}
+    <main className="relative h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/25 to-black">
+      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
+      <div className="h-screen flex justify-center items-center gap-3">
+        {appRoutes.map((route) => (
+          <Link key={route} href={route}>
+            {route}
+          </Link>
+        ))}
+      </div>
     </main>
   )
 }
