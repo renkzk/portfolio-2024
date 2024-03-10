f8a1cd0ebe7828995e8395961101341f57e0ca0f
renhubgit <renik2001@gmail.com>
2024-03-10 21:58:47 +0100

    contact section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,42 @@
+import { PinContainer } from "@/components/ui/3d-pin"
+import ContactCard from "@/components/ui/contact-card"
 import { NavigationBar } from "@/components/ui/navigation-bar"
+import { GithubIcon, LinkedinIcon, MailIcon } from "lucide-react"
+import Link from "next/link"
 import React from "react"
 
 export default function Contact() {
+  const iconSize = 100
+  const contactOptions = [
+    {
+      title: "LinkedIn",
+      icon: <LinkedinIcon size={iconSize} />,
+      link: "https://it.linkedin.com/in/renatokozak",
+    },
+    {
+      title: "Email",
+      icon: <MailIcon size={iconSize} />,
+      link: "mailto:renkzk.dev@gmail.com",
+    },
+    {
+      title: "GitHub",
+      icon: <GithubIcon size={iconSize} />,
+      link: "https://github.com/renkzk",
+    },
+  ]
+
   return (
     <>
       <NavigationBar />
+      <div className="flex flex-wrap justify-center items-center h-screen">
+        {contactOptions.map((contact) => (
+          <Link target="_blank" key={contact.link} href={contact.link}>
+            <PinContainer title={contact.link} href={contact.link}>
+              <ContactCard title={contact.title} icon={contact.icon} />
+            </PinContainer>
+          </Link>
+        ))}
+      </div>
     </>
   )
 }
---
+++ src/components/ui/contact-card.tsx
@@ -0,0 +1,19 @@
+import { LucideIcon } from "lucide-react"
+import React from "react"
+
+export default function ContactCard({
+  title,
+  icon,
+}: {
+  title: string
+  icon: JSX.Element
+}) {
+  return (
+    <div className="sm:w-[20rem] sm:h-[20rem] w-[10rem] h-[10rem] flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2  text-center">
+      <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">{title}</h3>
+      <div className="flex justify-center items-center flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-zinc-800 via-slate-700 to-slate-900">
+        {icon}
+      </div>
+    </div>
+  )
+}
