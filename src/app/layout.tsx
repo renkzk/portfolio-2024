0a973d788fd886afe1272709ed537199c131d8f7
renhubgit <renik2001@gmail.com>
2024-03-10 15:52:35 +0100

    added font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import LocalFont from "next/font/local"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
-
+// Metadata
 export const metadata: Metadata = {
   title: "renkzk",
   icons: {
@@ -11,13 +11,24 @@ export const metadata: Metadata = {
   },
 }
 
+// Fonts
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+})
+
+const calSans = LocalFont({
+  src: "../../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
       <body className={inter.className}>{children}</body>
     </html>
   )
